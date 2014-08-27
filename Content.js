03ef7abe797e752c58bbd901fdcb4070eadd4829
Mike Lee <mike.seung.lee@gmail.com>
2014-08-26 17:31:45 -0700

    modified to work on current mac

--- Content.js
+++ Content.js
@@ -1,11 +1,11 @@
-var allQuestions = [{question:'Who is the current president of the United States?', choices: ['Mitt Romney', 'Barack Obama', 'George Bush', 'John Kerry'], correctAnswer:0}, {question:'Who is the quarterback of the Michigan Wolverines?', choices: ['Devin Gardner', 'Denard Robinson', 'Shane Morris', 'Jeremy Gallon'], correctAnswer:0}, {question:'Who was the commencement speaker for the 2013 University of Michigan graduation ceremony?', choices:['Oprah Winfrey', 'Larry Page', 'Barack Obama', 'Dick Costolo'], correctAnswer:0}, {question:'What street do college students for late night food at the University of Michigan?', choices:['Main Street', 'State Street', 'East University Avenue', 'Plymouth Road'], correctAnswer:0}, {question:'What pizza company is located nearby the University of Michigan?', choices:['Papa Johns', 'Pizza House', 'Dominoes', 'Back Room Pizza'], correctAnswer:0}, {question:'What\'s the most expensive restaurant located in Ann Arbor?', choices:['Eastern Flame', 'Chop House', 'Frito Batidos', 'Wendy\'s']}];
-var totalQuestions = allQuestions.length;
+var allQuestions = [{question:'Who is the current president of the United States?', choices: ['Mitt Romney', 'Barack Obama', 'George Bush', 'John Kerry'], correctAnswer: 'Barack Obama'}, {question:'Who is the quarterback of the Michigan Wolverines?', choices: ['Devin Gardner', 'Denard Robinson', 'Shane Morris', 'Jeremy Gallon'], correctAnswer:'Devin Gardner'}, {question:'Who was the commencement speaker for the 2013 University of Michigan graduation ceremony?', choices:['Oprah Winfrey', 'Larry Page', 'Barack Obama', 'Dick Costolo'], correctAnswer:'Dick Costolo'}, {question:'What street do college students for late night food at the University of Michigan?', choices:['Main Street', 'State Street', 'East University Avenue', 'Plymouth Road'], correctAnswer:'East University Avenue'}, {question:'What pizza company is located nearby the University of Michigan?', choices:['Papa Johns', 'Pizza House', 'Dominoes', 'Back Room Pizza'], correctAnswer:'Dominoes'}, {question:'What\'s the most expensive restaurant located in Ann Arbor?', choices:['Eastern Flame', 'Chop House', 'Frito Batidos', 'Wendy\'s'], correctAnswer:'Chop House'}];
+var totalQNum = allQuestions.length;
 var navLocation = 0;
-var question = [];
+var questionList = [];
+var corAnswerList = []
 var allChoices = [];
 
-var userAnswer = [];
-var rightAnswer = [,1,0,3,2,2,1] //these are the numerical 1-indexes of the correct answer
+var userAnswerList = [];
 
 var createQuestion = function(n, q) {
   $('.questionCont').html(q[n]);
@@ -18,13 +18,7 @@ var checkAnswers = function(a, b) {
       correct++;
     }
   }
-  if (correct >= totalQuestions) {
-    correct = totalQuestions;
-    return totalQuestions;
-  }
-  else {
-    return correct;
-  }
+  return correct;
 };
 
 var createAnswers = function(a, n) {
@@ -48,25 +42,19 @@ var createAnswers = function(a, n) {
 $(function() {
   $('.results').hide();
   $('.progress').hide();
-  $('.totalQ').html(totalQuestions);
+  $('.totalQ').html(totalQNum);
   //parse the question array, allQuestions and grab the questions and the choices
   for (x in allQuestions) {
-    //should I put everything into the for/in loop
+    //for each object in allQuestions, append question list with the question and 
     question[x] = allQuestions[x].question;
     allChoices[x] = allQuestions[x].choices;
-    /*The following code creates a questionbox and radio choice button with answers in
-      all of them without adding them to the document yet
-      After looking it up, it would be simpler just to change whatever is in the
-      containers to the current question*/
-  }
-  
-  $('.navigation').onSubmit(function() {
+  };
 
-  });
   
   //the click needs to be simplified and moved out to other events
   //maybe make the click only a submit button
   //have other things listen for submit and switch the questions
+  
   $('.navigation').on('click', function() {
     if (navLocation != 0 && $('input:radio[name=choice]:checked').val() == undefined) {
       alert('you haven\'t picked an answer yet');
