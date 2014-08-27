d1be90e4d81d4193d6f64afd1a6dd1e1fe05ae89
Mike Lee <mike.seung.lee@gmail.com>
2014-08-26 22:24:20 -0700

    Working version, css file to come soon

--- Content.js
+++ Content.js
@@ -1,9 +1,9 @@
 var allQuestions = [{question:'Who is the current president of the United States?', choices: ['Mitt Romney', 'Barack Obama', 'George Bush', 'John Kerry'], correctAnswer: 'Barack Obama'}, {question:'Who is the quarterback of the Michigan Wolverines?', choices: ['Devin Gardner', 'Denard Robinson', 'Shane Morris', 'Jeremy Gallon'], correctAnswer:'Devin Gardner'}, {question:'Who was the commencement speaker for the 2013 University of Michigan graduation ceremony?', choices:['Oprah Winfrey', 'Larry Page', 'Barack Obama', 'Dick Costolo'], correctAnswer:'Dick Costolo'}, {question:'What street do college students for late night food at the University of Michigan?', choices:['Main Street', 'State Street', 'East University Avenue', 'Plymouth Road'], correctAnswer:'East University Avenue'}, {question:'What pizza company is located nearby the University of Michigan?', choices:['Papa Johns', 'Pizza House', 'Dominoes', 'Back Room Pizza'], correctAnswer:'Dominoes'}, {question:'What\'s the most expensive restaurant located in Ann Arbor?', choices:['Eastern Flame', 'Chop House', 'Frito Batidos', 'Wendy\'s'], correctAnswer:'Chop House'}];
-var totalQNum = allQuestions.length;
-var navLocation = 0;
+var totalNumQ = allQuestions.length;
+var nav = -1;
 var questionList = [];
 var corAnswerList = []
-var allChoices = [];
+var choiceList = [];
 
 var userAnswerList = [];
 
@@ -21,7 +21,7 @@ var checkAnswers = function(a, b) {
   return correct;
 };
 
-var createAnswers = function(a, n) {
+var createAnswers = function(n, a) {
   $('.answerCont').html(''); //removes anything in .answerCont and replaces it with ''
   for (s in a[n]) { //Creates 
     var name = 'choice' + s;
@@ -30,7 +30,7 @@ var createAnswers = function(a, n) {
     aChoice.appendTo('.answerCont');
     aChoice.html(a[n][s]);
     var radioButton = $('<input>');
-    radioButton.attr({type:'radio',value:s, name:'choice'});
+    radioButton.attr({type:'radio', value:a[n][s], name:'choice'});
     radioButton.attr('class', name);
     radioButton.html(a[n][s])
     radioButton.prependTo(aChoice);
@@ -42,12 +42,13 @@ var createAnswers = function(a, n) {
 $(function() {
   $('.results').hide();
   $('.progress').hide();
-  $('.totalQ').html(totalQNum);
+  $('.totalQ').html(totalNumQ);
   //parse the question array, allQuestions and grab the questions and the choices
   for (x in allQuestions) {
-    //for each object in allQuestions, append question list with the question and 
-    question[x] = allQuestions[x].question;
-    allChoices[x] = allQuestions[x].choices;
+    /*for each object in allQuestions, append question list, choices list, and correct answer list with the corresponding strings from the question bank, allQuestions*/
+    questionList[x] = allQuestions[x].question;
+    choiceList[x] = allQuestions[x].choices;
+    corAnswerList[x] = allQuestions[x].correctAnswer;
   };
 
   
@@ -56,41 +57,32 @@ $(function() {
   //have other things listen for submit and switch the questions
   
   $('.navigation').on('click', function() {
-    if (navLocation != 0 && $('input:radio[name=choice]:checked').val() == undefined) {
-      alert('you haven\'t picked an answer yet');
-      return;
-    }
-    else if (navLocation >= totalQuestions) {
-      var userPick = $('input:radio[name=choice]:checked').val();
-      userAnswer[navLocation] = userPick;
-      $('#correctResults').html(checkAnswers(rightAnswer, userAnswer));
-      $('.results').show();
-      $('.progress').hide();
-      $('.questionCont').hide();
-      $('.answerCont').hide();
-      $('#currentQ').html(totalQuestions);
-      $('.navigation').hide();
-      return;
-    }
-    else {
+    if (nav == -1) {
       $('.welcome').hide();
       $('.progress').show();
-      if (navLocation == 0) {
-        createQuestion(navLocation, question)
-        createAnswers(allChoices, navLocation)
-        $('#currentQ').html(navLocation+1);
-        navLocation++;
-      }
-      else {
-        var userPick = $('input:radio[name=choice]:checked').val();
-        userAnswer[navLocation] = userPick;
-        createQuestion(navLocation, question);
-        createAnswers(allChoices, navLocation);
-        $('#currentQ').html(navLocation+1);
-        navLocation++;
-      }
+      nav = 0;
+      createQuestion(nav, questionList);
+      createAnswers(nav, choiceList);
     }
-
-    
+    else if (nav+1>=totalNumQ) {
+      var userPick = $('input:checked').val();
+      userAnswerList[nav] = userPick
+      $('.progress').hide()
+      $('.content').hide();
+      $('navigation').hide();
+      $('#correctResults').html(checkAnswers(corAnswerList, userAnswerList));
+      $('.results').show();
+    }
+    else if (nav !=0 && $('input:checked').val() == undefined) {
+      alert('you haven\'t picked an answer yet');
+    }
+    else {
+      var userPick = $('input:checked').val();
+      userAnswerList[nav] = userPick;
+      nav++;
+      $('#currentQ').html(nav+1);
+      createQuestion(nav, questionList);
+      createAnswers(nav, choiceList);
+    }
   })
-})
+});
